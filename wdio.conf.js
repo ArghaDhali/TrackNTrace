4d5d0a3d877b52153986c7e2b4d214054a6e6b49
ArghaDhali <126243384+ArghaDhali@users.noreply.github.com>
2023-03-23 15:34:53 +0530

    Update wdio.conf.js

--- wdio.conf.js
+++ wdio.conf.js
@@ -26,13 +26,13 @@ exports.config = {
     specs: [
         //'./test/specs/**/*.js'
         //'./test/specs/UserRegistration.js'
-        //'./test/specs/Login.js'
+        './test/specs/Login.js'
         //'./test/specs/Dashboard.js'
         //'./test/specs/InitiateWorkflow.js'
         //'./test/specs/BulkOrderTokens.js'
         //'./test/specs/Distributors.js'
         //'./test/specs/OrderTokenTables.js'
-        './test/specs/QRCode.js'
+        //'./test/specs/QRCode.js'
     ],
     // Patterns to exclude.
     exclude: [
